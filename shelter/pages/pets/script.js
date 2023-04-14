ea0753cb7c46e4be8bf8409bbdb4144f831267d1
ÑPaul Sorkin <sorkinpavel@mail.ru>
2023-04-14 22:35:23 +0300

    refactor: burger menu

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,27 +1,31 @@
+import pets from '../../assets/pets.json' assert { type: "json" }
+
+//Burger====
 const burger = document.querySelector('.burger');
 const smallNav = document.querySelector('.small__nav');
 const smallNavList = document.querySelector('.small__nav__list');
 const smallNavItems = document.querySelectorAll('.small_nav_link');
 const body = document.querySelector('body');
-
+const darken = document.querySelector('.darken');
 
 function toggleMenu() {
     smallNav.classList.toggle('open');
-    if (smallNav.classList.contains('open')) {
-        body.classList.add('no-overflow');
-    } else {
-        body.classList.remove('no-overflow');
-    }
+    body.classList.toggle('no-overflow');
+    darken.classList.toggle('darken-active');
 }
 function activeBurger() {
     burger.classList.toggle('active-burger');
 }
 function closeMenu(event) {
-    if (event.target.classList.contains('small_nav_link')) {
+    if (event.target.classList.contains('small_nav_link') || event.target.classList.contains('darken')) {
         smallNav.classList.remove('open');
         burger.classList.remove('active-burger');
+        body.classList.remove('no-overflow');
+        darken.classList.remove('darken-active');
     }
 }
+
 burger.addEventListener('click', toggleMenu);
 burger.addEventListener('click', activeBurger);
 smallNavList.addEventListener('click', closeMenu);
+darken.addEventListener('click', closeMenu);
